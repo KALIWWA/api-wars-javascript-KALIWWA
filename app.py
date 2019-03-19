8e7be6d73bc9f004ec2e9371ed4712e07972f1fb
Michal Kalinowski <subaru.kali@gmail.com>
2019-03-19 11:52:24 +0100

    favicon set

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -8,8 +9,14 @@ def route_index():
     return render_template('index.html')
 
 
+@app.route('/favicon.ico')
+def route_favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'stormTrooper.ico', mimetype='image/vnd.microsoft.icon')
+
+
 if __name__ == '__main__':
     app.run(
         debug=True,
-        port=5000
+        port=8000
     )
