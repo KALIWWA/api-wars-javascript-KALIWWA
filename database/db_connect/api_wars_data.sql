ec63133b1ea5ce90589eae923e92cdf60d8d647e
Michal Kalinowski <subaru.kali@gmail.com>
2019-03-19 11:46:57 +0100

    database set

---
+++ database/db_connect/api_wars_data.sql
@@ -0,0 +1,33 @@
+ALTER TABLE IF EXISTS ONLY public.users
+  DROP CONSTRAINT IF EXISTS pk_users_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.planet_votes
+  DROP CONSTRAINT IF EXISTS pk_planet_votes_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.planet_votes
+  DROP CONSTRAINT IF EXISTS fk_user_id CASCADE;
+
+DROP TABLE IF EXISTS public.users;
+CREATE TABLE users
+(
+  id       SERIAL PRIMARY KEY NOT NULL,
+  username VARCHAR(50) UNIQUE NOT NULL,
+  password VARCHAR(200)       NOT NULL
+);
+
+DROP TABLE IF EXISTS public.planet_votes;
+CREATE TABLE planet_votes
+(
+  id              SERIAL PRIMARY KEY NOT NULL,
+  planet_id       INTEGER            NOT NULL,
+  planet_name     VARCHAR(50),
+  user_id         INTEGER,
+  submission_time TIMESTAMP WITHOUT TIME ZONE
+);
+
+
+ALTER TABLE ONLY planet_votes
+  ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id);
+
+
+SELECT pg_catalog.setval('users_id_seq', (SELECT MAX(id) from "users"), true);
+
+SELECT pg_catalog.setval('planet_votes_id_seq', (SELECT MAX(id) from "planet_votes"), true);
