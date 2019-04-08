22d18bff05906b6048b088a3c8e2d57e2994518c
Michal Kalinowski <subaru.kali@gmail.com>
2019-04-08 13:13:53 +0200

    modal structure continue

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,6 +1,13 @@
+function hideModal() {
+	const modal = document.querySelector('.modal');
+
+	modal.removeAttribute('display');
+	modal.setAttribute('display', 'none');
+}
+
 function showModal() {
 	const modal = document.querySelector('.modal');
 
-	modal.removeAttribute('display', 'none');
+	modal.removeAttribute('display');
 	modal.setAttribute('display', 'block');
 }
