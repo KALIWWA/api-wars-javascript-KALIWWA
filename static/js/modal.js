632c3a6f17e2ccc5f9433962675eff9789c15e0b
Michal Kalinowski <subaru.kali@gmail.com>
2019-04-08 20:11:26 +0200

    modal show & create/remove working

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,13 +1,59 @@
-function hideModal() {
-	const modal = document.querySelector('.modal');
+// //show modal
+//
+// const modalBtn = document.getElementById('modalBtn');
+// modalBtn.addEventListener('click', function () {
+//
+// 	const modalBg = document.querySelector('.modal-bg');
+// 	modalBg.style.display = 'flex';
+// });
+//
+//
+// const closeBtn = document.querySelector('.close-modal');
+// closeBtn.addEventListener('click', function () {
+//
+// 	const modalBg = document.querySelector('.modal-bg');
+// 	modalBg.style.display = 'none';
+// });
 
-	modal.removeAttribute('display');
-	modal.setAttribute('display', 'none');
+// create modal
+
+const modalBtn = document.getElementById('modalBtn');
+modalBtn.addEventListener('click', () => createModal());
+
+function createModal() {
+	const modalPlace = document.getElementById('modalPlace');
+	const modalBg = document.createElement('div');
+	modalBg.classList.add('modal-bg');
+	modalPlace.appendChild(modalBg);
+	
+	const modalContent = document.createElement('div');
+	modalContent.classList.add('modal-content');
+	modalBg.appendChild(modalContent);
+
+	const modalClose = document.createElement('div');
+	modalClose.classList.add('close-modal');
+	modalClose.innerText = '+';
+	modalContent.appendChild(modalClose);
+
+	const modalTitle = document.createElement('h1');
+	modalTitle.innerText = 'Test Modal';
+	modalContent.appendChild(modalTitle);
+
+	modalClose.addEventListener('click', () => removeModal());
+	window.addEventListener('click', outsideClick);
 }
 
-function showModal() {
-	const modal = document.querySelector('.modal');
+function removeModal() {
+	const modalPlace = document.getElementById('modalPlace');
 
-	modal.removeAttribute('display');
-	modal.setAttribute('display', 'block');
+	while (modalPlace.firstChild) {
+		modalPlace.removeChild(modalPlace.firstChild)
+	}
+}
+
+function outsideClick(e) {
+	const modalBg = document.querySelector('.modal-bg');
+	if (e.target === modalBg) {
+		modalBg.remove()
+	}
 }
